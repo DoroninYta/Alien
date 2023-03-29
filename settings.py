6a7f05ce1961f0f784453dc4dfa5e8fa1e3f572c
DoroninYta <critek123@mail.ru>
2023-03-29 20:40:24 +0300

    ships can be destroyed

--- settings.py
+++ settings.py
@@ -5,9 +5,15 @@ class Settings():
         self.bg_color = (255, 255, 255)
 
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
-        self.bullet_speed = 1
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 10
+
+        self.alien_speed = 0.8
+        self.fleet_drop_speed = 50 #down speed
+        self.fleet_direction = 1 #1 or -1 right or left
+
--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship():
     def __init__(self, ai_game):
         self.screen = ai_game.screen
@@ -7,6 +8,7 @@ class Ship():
 
         self.image = pygame.image.load("images/ship.bmp").convert_alpha()
         self.image = pygame.transform.scale(self.image, (100, 80))
+        
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
@@ -16,13 +18,21 @@ class Ship():
         self.settings = ai_game.settings
         self.x = float(self.rect.x)
 
+
     def update(self):
-        if self.moving_right and self.rect.right < self.screen_rect.right + 70:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        elif self.moving_left and self.rect.left > -70:
+
+        elif self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        
         self.rect.x = self.x
+
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
         
